268f999c32a1981097a2e20594e041a42f80dd92
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2024-10-14 12:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // your code here
+let text= document.getElementById("url")
+let name = document.getElementById("name")
+let year= document.getElementById("year")
+
+let button= document.getElementById("button")
+button.addEventListener('click',()=>{
+    console.log(name.value, year.value)
+    if(name.value !== "" && year.value !==""){
+     return   text.innerText=`${text.innerText}/?name=${name.value}/year=${year.value}`
+    }else if(year.value ===""){
+     return   text.innerText=`${text.innerText}/?name=${name.value}`
+    }
+    else {
+     return   text.innerText=`${text.innerText}/?year=${year.value}`
+    }
+})
+
